17298806ff3f181d9b4d367a472d554d74e5e1e1
Isheanesu Gwangwanyu <38261770+ishe19@users.noreply.github.com>
2023-09-04 11:07:19 +0200

    Update search_tab.dart

--- lib/ui/home/screens/tabs/search/search_tab.dart
+++ lib/ui/home/screens/tabs/search/search_tab.dart
@@ -3,7 +3,8 @@ import 'package:dvt_weather/data/models/searched_up_locations.dart';
 import 'package:dvt_weather/ui/home/screens/tabs/search/widgets/searched_up_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:location_picker_flutter_map/location_picker_flutter_map.dart';import '../../../../constants/constants.dart';
+import 'package:location_picker_flutter_map/location_picker_flutter_map.dart';
+import '../../../../constants/constants.dart';
 
 class SearchTab extends StatefulWidget {
   const SearchTab({super.key});
@@ -12,42 +13,51 @@ class SearchTab extends StatefulWidget {
   State<SearchTab> createState() => _SearchTabState();
 }
 
-class _SearchTabState extends State<SearchTab> with AutomaticKeepAliveClientMixin<SearchTab> {
+class _SearchTabState extends State<SearchTab>
+    with AutomaticKeepAliveClientMixin<SearchTab> {
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    final searchedLocationsBloc = BlocProvider.of<SearchedLocationsBloc>(context, listen: false);
+    final searchedLocationsBloc =
+        BlocProvider.of<SearchedLocationsBloc>(context, listen: false);
     return SizedBox(
-      height: screenHeight(context),
-      child: Column(
-        children: [
-          Expanded(
-            child: FlutterLocationPicker(
-                initZoom: 11,
-                minZoomLevel: 5,
-                maxZoomLevel: 16,
-                trackMyPosition: true,
-                onPicked: (pickedData) {
-                  SearchedUpLocation location = SearchedUpLocation(name: pickedData.address,
-                      longitude: "${pickedData.latLong.longitude}", latitude: "${pickedData.latLong.latitude}");
-                  searchedLocationsBloc.add(AddLocationEvent(location));
-                }),
-          ),
-          Expanded(
-            child: BlocBuilder<SearchedLocationsBloc, SearchedLocationsState>(
-              builder: (context, state){
-            return ListView.builder(
-              itemCount: state.locations.length,
-              itemBuilder: (context, index){
-                return SearchedUpLocationTile(location: state.locations[index],);
-              },
-            );
-},
+        height: screenHeight(context),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: FlutterLocationPicker(
+                  selectLocationButtonText: "Add Current Location",
+                  initZoom: 11,
+                  minZoomLevel: 5,
+                  maxZoomLevel: 16,
+                  trackMyPosition: true,
+                  onPicked: (pickedData) {
+                    SearchedUpLocation location = SearchedUpLocation(
+                        name: pickedData.address,
+                        longitude: "${pickedData.latLong.longitude}",
+                        latitude: "${pickedData.latLong.latitude}");
+                    searchedLocationsBloc.add(AddLocationEvent(location));
+                  }),
             ),
-          )
-        ],
-      )
-    );
+            Expanded(
+              flex: 1,
+              child: BlocBuilder<SearchedLocationsBloc, SearchedLocationsState>(
+                builder: (context, state) {
+                  return ListView.builder(
+                    padding: const EdgeInsets.only(bottom: 15),
+                    itemCount: state.locations.length,
+                    itemBuilder: (context, index) {
+                      return SearchedUpLocationTile(
+                        location: state.locations[index],
+                      );
+                    },
+                  );
+                },
+              ),
+            )
+          ],
+        ));
   }
 
   @override
