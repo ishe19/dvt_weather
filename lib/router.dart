74cff40c8e446c8e18f80490c85295ce5d9f345b
Isheanesu Gwangwanyu <38261770+ishe19@users.noreply.github.com>
2023-09-04 15:04:59 +0200

    Update router.dart

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,5 @@
-import 'package:dvt_weather/ui/home/screens/home_screen.dart';
-import 'package:dvt_weather/ui/settings/settings_screen.dart';
+import 'package:dvt_weather/presentation/home/screens/home_screen.dart';
+import 'package:dvt_weather/presentation/settings/settings_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
