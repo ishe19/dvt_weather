33455c8eea1f2d13cd4f751fd2607c7eab2ab895
Isheanesu Gwangwanyu <38261770+ishe19@users.noreply.github.com>
2023-09-05 10:27:09 +0200

    Create export_models.dart

---
+++ lib/data/models/export_models.dart
@@ -0,0 +1,7 @@
+export 'current_weather_model.dart';
+export 'favourite_model.dart';
+export 'list_element_model.dart';
+export 'main_model.dart';
+export 'rain_model.dart';
+export 'searched_up_locations.dart';
+export 'weather_model.dart';
