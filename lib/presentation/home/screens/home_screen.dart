0ae850e857350b0e3ddef22f91b4d24631b88994
Isheanesu Gwangwanyu <38261770+ishe19@users.noreply.github.com>
2023-09-05 08:56:18 +0200

    Update home_screen.dart

--- lib/presentation/home/screens/home_screen.dart
+++ lib/presentation/home/screens/home_screen.dart
@@ -24,7 +24,6 @@ class _HomeScreenState extends State<HomeScreen> {
     bool serviceEnabled;
     LocationPermission permission;
 
-    // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       return Future.error('Location services are disabled.');
