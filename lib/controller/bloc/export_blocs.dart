f2631017689a16123a5b3c559d55a47d9029c8df
Isheanesu Gwangwanyu <38261770+ishe19@users.noreply.github.com>
2023-09-05 11:00:46 +0200

    Create export_blocs.dart
    made an export file for my blocs

---
+++ lib/controller/bloc/export_blocs.dart
@@ -0,0 +1,6 @@
+export 'app_bar_controller/app_bar_controller_bloc.dart';
+export 'current_weather/current_weather_bloc.dart';
+export 'favourites/favourites_bloc.dart';
+export 'searched_locations/searched_locations_bloc.dart';
+export 'weather_ui/weather_ui_bloc.dart';
+export 'weather_data/weather_data_bloc.dart';
